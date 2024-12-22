c805b6198b885616887c04310ca020848e5f0fbd
Akathi <akathiyanp256@gmail.com>
2024-12-22 19:13:21 +0530

    publish 3rd time

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,20 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
+
+// export default defineConfig({
+//   base:"/deploy-github/",
+//   plugins: [react()],
+// })
+// vite.config.ts
+// vite.config.ts
+import { defineConfig } from 'vite';
+
 export default defineConfig({
-  base:"/deploy-github/",
-  plugins: [react()],
-})
+  base: '/akathi-spotify/', // Ensure this matches your GitHub repository name
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+  }
+});
